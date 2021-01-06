5798ff01a2549d335e29834c3f20be0dfa6f7482
Mario Vellandi <mvellandi@hotmail.com>
2021-01-06 11:31:49 +0100

    first pass

---
+++ nav.js
@@ -0,0 +1,17 @@
+function setNavigation(num) {
+  return {
+    mobileNavOpen: false,
+    activeNavItem: num,
+    navItems: [
+      { id: 0, title: "Cover", url: "../index.html" },
+      { id: 1, title: "One", url: "1.html" },
+      { id: 2, title: "Two", url: "2.html" },
+      { id: 3, title: "Three", url: "3.html" },
+      { id: 4, title: "Four", url: "4.html" },
+      { id: 5, title: "Five", url: "5.html" },
+      { id: 6, title: "Six", url: "6.html" },
+      { id: 7, title: "Seven", url: "7.html" },
+      { id: 8, title: "Eight", url: "8.html" },
+    ],
+  };
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'xs': "360px",
+      'sm': '600px',
+      'md': '800px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
