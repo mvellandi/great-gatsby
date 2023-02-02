00b3cfab52e4d6a4ee78f1509cba3f98a62f3ebd
Mario Vellandi <mvellandi@hotmail.com>
2023-02-02 22:01:38 +0100

    ~ Upgraded tailwind ~ minor formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      'xs': "360px",
-      'sm': '600px',
-      'md': '800px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      xs: "360px",
+      sm: "600px",
+      md: "800px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     extend: {},
   },
